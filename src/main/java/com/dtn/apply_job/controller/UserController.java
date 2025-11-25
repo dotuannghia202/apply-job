349ea3a7b1e18d0c7f9f8059cef297fb7371a74b
tuannghia202 <tuannghia3nest@gmail.com>
2025-11-25 19:53:16 +0700

    CRUD User

--- src/main/java/com/dtn/apply_job/controller/UserController.java
+++ src/main/java/com/dtn/apply_job/controller/UserController.java
@@ -2,11 +2,9 @@ package com.dtn.apply_job.controller;
 
 import com.dtn.apply_job.domain.User;
 import com.dtn.apply_job.service.UserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,17 +17,34 @@ public class UserController {
     public UserController(UserService userService) {
         this.userService = userService;
     }
-//    @GetMapping("/users")
-//    public List<User> getAllUsers() {
-//        return this.userService.getAllUsers();
-//    }
 
-    @GetMapping ("/users")
-    public User createUser() {
-        User user = new User();
-        user.setName("Đỗ Tuấn Nghĩa");
-        user.setPassword("123456");
-        user.setEmail("dotuannghia07@gmail.com");
-        return this.userService.createUser(user);
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> result = this.userService.getAllUsers();
+        return ResponseEntity.ok().body(result);
+    }
+
+    @PostMapping ("/users")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User result = this.userService.createUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(null);
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable long id) {
+        User result = this.userService.getUserById(id);
+        return ResponseEntity.ok().body(result);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<Void> deleteUserById(@PathVariable long id) {
+        this.userService.deleteUserById(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @PutMapping("/users/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable long id , @RequestBody User user) {
+        User result = this.userService.updateUser(id, user);
+        return ResponseEntity.ok().body(result);
     }
 }
--- src/main/java/com/dtn/apply_job/service/UserService.java
+++ src/main/java/com/dtn/apply_job/service/UserService.java
@@ -21,4 +21,20 @@ public class UserService {
     public User createUser(User user) {
         return this.userRepository.save(user);
     }
+
+    public User getUserById(long id) {
+        return this.userRepository.findById(id).get();
+    }
+
+    public void deleteUserById(long id) {
+        this.userRepository.deleteById(id);
+        return;
+    }
+
+    public User updateUser(long id, User newUser) {
+        User userUpdate = getUserById(id);
+        userUpdate.setName(newUser.getName());
+        userUpdate.setPassword(newUser.getPassword());
+        return this.userRepository.save(userUpdate);
+    }
 }
