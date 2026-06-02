ca57a1a199e2432f4ae71132ea32ce37f4e50bcd
tuannghia202 <tuannghia3nest@gmail.com>
2026-06-02 15:50:09 +0700

    Add response DTO: create ResUpdateApplicationDTO for updating application status with ID and status fields

---
+++ src/main/java/com/dtn/apply_job/domain/response/application/ResUpdateApplicationDTO.java
@@ -0,0 +1,13 @@
+package com.dtn.apply_job.domain.response.application;
+
+import com.dtn.apply_job.util.constant.enums.ApplicationStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ResUpdateApplicationDTO {
+    private Long id;
+    private ApplicationStatus status;
+}
+
